f173b2a64d1aa78d13a87c05ae5b0c709a6c043f
kwonjjinho <109893466+kwonjjinho@users.noreply.github.com>
2023-04-04 14:10:17 +0900

    Create get_num_square.py

---
+++ .github/workflows/get_num_square.py
@@ -0,0 +1,12 @@
+import os
+
+num = os.environ.get("INPUT_NUM")
+if num:
+    try:
+        num = int(num)
+    except Exception:
+        exit('ERROR: the INPUT-NUM provided ("{}")is npt ')
+else:
+    num = 1
+    
+print(f"::set-output name=num_squared::{num ** 2}")
